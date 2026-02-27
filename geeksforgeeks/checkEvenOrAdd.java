882603b44d3f22f080f20a703064fd94da87ba2a
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-02-27 22:25:04 +0530

    DSA

---
+++ geeksforgeeks/checkEvenOrAdd.java
@@ -0,0 +1,16 @@
+
+public class checkEvenOrAdd {
+    public static void main(String[] args) {
+
+        int n =15;
+
+        if(n%2==0){
+            System.out.println("it is even number");
+        }
+        else{
+            System.out.println("it is odd number ");
+        }
+
+
+    }
+}

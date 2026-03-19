43f21eabcc1e35092e8290abb63cdc9f402ec636
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-03-19 07:06:24 +0530

    DSA

--- Patterns/PascalTriangle.java
+++ Patterns/PascalTriangle.java
@@ -5,10 +5,11 @@ public class PascalTriangle {
         
         int n =5;
         for(int i=0;i<=n;i++){
-            int num =1;
+            
             for(int s=0;s<n-i;s++){
                 System.out.print(" ");
             }
+            int num =1;
             for(int j=0;j<=i;j++){
                 System.out.print(num+" ");
                 num = num*(i-j)/(j+1);
